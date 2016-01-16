d2cdf3a5f7f60e243ff8053ec4cb5564989eb4e7
Thomas Mault <tmault@Thomass-MacBook-Pro.local>
2016-01-16 18:26:17 +0000

    Progressing Lesson 2.2

--- Stage 2/strings.py
+++ Stage 2/strings.py
@@ -32,3 +32,64 @@ print 'Hello' + name + '!'
 
 
 # Lesson 2.2.: Indexing Strings
+
+# Write Python code that prints out Udacity (with a capital U), 
+# given the definition of s below.
+
+s = 'audacity'
+u = s[1]
+print u.upper() + s[2:] 
+
+
+# This segment is just a chance for you to play around with 
+# finding strings within strings. Read through the code and 
+# press Test Run to see what it does. Is there anything 
+# interesting or unexpected?
+
+print "Example 1: Finding substrings in a string"
+print "test".find("te")
+print "test".find("st")
+print "test"[2:]
+
+print "Example 2: Finding substrings in a string which is stored as a variable"
+my_string = "test"
+print my_string.find("te")
+print my_string.find("st")
+print my_string[2:]
+
+print "Example 3: Printing out everything after a certain substring"
+my_string = "My favorite color: blue"
+color_start_location = my_string.find("color:")
+favorite_color = my_string[color_start_location:]
+print favorite_color # oops, this line prints out 'color: ' as well...
+print favorite_color[7:] # this fixes it!
+
+print "Example 4: Other interesting things about string.find()..."
+print "text".find("text") # prints 0
+print "text".find("Text") # prints -1
+print "text".find("")     # prints 0
+print "text".find(" ")    # prints -1  
+
+
+
+# This segment is just a chance for you to play around with 
+# finding strings within strings. Read through the code and 
+# press Test Run to see what it does. Is there anything 
+# interesting or unexpected?
+
+print "Example 1: using find to print the second occurrence of a sub-string"
+print "test".find("t") # I expect this to print 0
+print "test".find("t", 1) # I expect this to print 3
+
+print "Example 2: using a variable to store first location"
+first_location = "test".find("t") # here we store the first location of "t"
+print "test".find("t", first_location+1) # then we use that location to find the second occurrence.
+
+print "Example 3: using find to get rid of exclamation marks!!"
+example = "Wow! Python is great! Don't you think?"
+first = example.find('!')
+second = example.find('!', first + 1)
+new_string = example[:first] + example[first+1:second] + example[second+1:]
+print new_string # oops, I should probably replace the !s with periods
+new_string = example[:first] +'.'+ example[first+1:second] +'.'+ example[second+1:]
+print new_string
